e2f07a011e3a2bebb17f337aea022c265a168f90
JeandersonSantos <jeandersonsantospaz@gmail.com>
2024-03-16 10:01:34 -0300

    pastas, css, script

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@ function pageWall() {
   let novoCaminho = caminhoAtual.replace("/home/", "/parede/");
   window.location.href = novoCaminho;
 }
+
+function pageLaje() {
+  let caminhoAtual = window.location.pathname;
+  let novoCaminho = caminhoAtual.replace("/home/", "/laje/");
+  window.location.href = novoCaminho;
+}
+
+function pageTelhado() {
+  let caminhoAtual = window.location.pathname;
+  let novoCaminho = caminhoAtual.replace("/home/", "/telhado/");
+  window.location.href = novoCaminho;
+}
+
+function pagePiso() {
+  let caminhoAtual = window.location.pathname;
+  let novoCaminho = caminhoAtual.replace("/home/", "/piso/");
+  window.location.href = novoCaminho;
+}
+
