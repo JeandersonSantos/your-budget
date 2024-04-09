80402e4e18d9df090104f29095afddda7b1dd0eb
JeandersonSantos <jeandersonsantospaz@gmail.com>
2024-04-08 23:37:47 -0300

    movendo página principal para a raiz do projeto, ajustando caminhos e funções

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 function pageWall() {
   let caminhoAtual = window.location.pathname;
-  let novoCaminho = caminhoAtual.replace("/home/", "/parede/");
+  let novoCaminho = `pages/parede${caminhoAtual}`;
   window.location.href = novoCaminho;
 }
 
 function pageLaje() {
   let caminhoAtual = window.location.pathname;
-  let novoCaminho = caminhoAtual.replace("/home/", "/laje/");
+  let novoCaminho = `pages/laje${caminhoAtual}`;
   window.location.href = novoCaminho;
 }
 
 function pageTelhado() {
   let caminhoAtual = window.location.pathname;
-  let novoCaminho = caminhoAtual.replace("/home/", "/telhado/");
+  let novoCaminho = `pages/telhado${caminhoAtual}`;
   window.location.href = novoCaminho;
 }
 
 function pagePiso() {
   let caminhoAtual = window.location.pathname;
-  let novoCaminho = caminhoAtual.replace("/home/", "/piso/");
+  let novoCaminho = `pages/piso${caminhoAtual}`;
   window.location.href = novoCaminho;
 }
 
+function back() {
+  let novoCaminho = "/index.html";
+  window.location.href = novoCaminho;
+}
