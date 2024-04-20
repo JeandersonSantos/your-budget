7403da01cd63d4daec7eb3ec95f4a380d69baddc
JeandersonSantos <jeandersonsantospaz@gmail.com>
2024-04-20 00:22:46 -0300

    função para formatar número com mais de duas casas decimais

--- js/script.js
+++ js/script.js
@@ -23,3 +23,15 @@ function back() {
   let novoCaminho = caminhoAtual.replace(/pages\/.*$/, "");
   window.location.href = `${novoCaminho}index.html#menu`;
 }
+
+function formatNumber(number) {
+  if (
+    Number.isFinite(number) &&
+    !Number.isInteger(number) &&
+    number.toString().split(".")[1].length > 1
+  ) {
+    return number.toFixed(2);
+  } else {
+    return number;
+  }
+}
